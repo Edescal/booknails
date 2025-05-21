167de646fae99e568074f24d3c79b3ce06155263
E. Escalante <eduardo1582000@gmail.com>
2025-05-20 18:43:50 -0600

    try static settings

--- booknails/settings.py
+++ booknails/settings.py
@@ -198,9 +198,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'
 
-# STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -209,9 +207,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'   # Carpeta donde Django va a colectar todos los estáticos para producción
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'   # Carpeta donde Django va a colectar todos los estáticos para producción
 
+STATIC_URL = 'static/'
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
